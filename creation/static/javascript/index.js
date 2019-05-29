7af7e34ad70a0d6115dc0f895dbd1562121a9188
AugustineOchieng <gusochieng@gmail.com>
2019-05-29 17:33:45 +0300

    Create javascript for circles on jumbotron

--- creation/static/javascript/index.js
+++ creation/static/javascript/index.js
@@ -1,3 +1,12 @@
 $(function () {
   $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
+
+(function ($) {
+$('.third.circle').circleProgress({
+  value: 0.75,
+  fill: { gradient: [['#0681c4', .5], ['#4ac5f8', .5]], gradientAngle: Math.PI / 4 }
+}).on('circle-animation-progress', function (event, progress, stepValue) {
+  $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+});
+})(jQuery);
