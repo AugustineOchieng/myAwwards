b58c5bcd1333a7b3d3c419cfc26891e49f5af5d8
AugustineOchieng <gusochieng@gmail.com>
2019-05-29 17:50:58 +0300

    Create new redirect to and from comment form

--- creation/urls.py
+++ creation/urls.py
@@ -11,12 +11,14 @@ urlpatterns=[
     url(r'^new_profile/$',views.new_profile,name = 'NewProfile'),
     url(r'^edit_profile/$',views.edit_profile,name = 'edit_profile'),
     url(r'^new/project/$', views.new_project, name='new_post'),
-    url(r'^new_comment/$', views.new_comment, name='comment'),
+    url(r'^single/new_comment/(\d+)/', views.new_comment, name='new_comment'),
     url(r'^comment/$',views.comment,name = 'comments'),
     
     url(r'^single/(\d+)', views.single, name='one_project'),
     url(r'^voter/$', views.voter, name='voting'),
-    url(r'^review/(?P<pk>\d+)',views.add_review,name='review'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/profile/$', views.Profilel.as_view()),
+    url(r'^api/project/$', views.Projecter.as_view())
     
     # url(r'^image/(\d+)',views.image,name ='image'),
   ]
